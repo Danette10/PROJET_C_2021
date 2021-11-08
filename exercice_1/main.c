ecc97ef31e37eed5b04cb93e0bdd0de1fc4833b5
Danette10 <38246813+Danette10@users.noreply.github.com>
2021-11-08 21:46:45 +0100

    Update main.c

--- exercice_1/main.c
+++ exercice_1/main.c
@@ -7,26 +7,44 @@
 int main() {
     char retry;
     do {
-        int nb, i,j;
+        int nb, i,j, impair, pair;
         printf("Saisir un entier :\n");
         scanf("%d", &nb);
         for (i = 0; i < nb - i - 1; i++){
             for (j = 0; j < nb; j++){
                 if(j <= i || j >= nb - i -1){
-                    printf("*");
+                    printf("* ");
                 } else{
-                    printf(" ");
+                    printf("  ");
                 }
 
             }
             printf("\n");
         }
+        for(impair=0;impair <= nb && nb%2 != 0;impair++)
+        {
+
+            if(impair == nb){
+                printf("\n");
+            } else
+                printf("* ");
+        }
+
+        for(pair=0;pair <= nb && nb%2 == 0;pair++)
+        {
+
+            if(pair == nb){
+                printf("\n");
+            } else
+                printf("* ");
+        }
+
         for (i = nb - i - 1; i > 0; i--){
             for (j = nb; j > 0; j--){
                 if(j <= i || j >= nb - i + 1){
-                    printf("*");
+                    printf("* ");
                 } else{
-                    printf(" ");
+                    printf("  ");
                 }
 
             }
