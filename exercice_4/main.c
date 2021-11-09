31ddda9a52dcf529c3c6e633de3bb9d7d6b7d73e
Danette10 <38246813+Danette10@users.noreply.github.com>
2021-11-09 23:07:29 +0100

    Update main.c

--- exercice_4/main.c
+++ exercice_4/main.c
@@ -1,49 +1,39 @@
 #include <stdio.h>
-#include <math.h>
-/*
- * Intervalle nombres sympathiques
- * par Dan Sebag
- * le 01/11/2021
- */
+
 int main() {
-    unsigned int nb1, nb2;
-
-
-    printf("Saisir un premier entier :\n");
-    scanf("%u",&nb1);
+    int nb1, nb2;
+    printf("Saisir un premier entier : \n");
+    scanf("%d", &nb1);
     printf("Saisir un second entier :\n");
-    scanf("%u",&nb2);
+    scanf("%d", &nb2);
 
     do {
-        int pow2 = 0, saveNb1, rest, p, binary=0;
-        unsigned int counter1 = 0, counter0 = 0;
-        
+        int rest, i = 0, counter0 = 0, counter1 = 0, missing0, saveNb1, temp1 = 0;
         saveNb1 = nb1;
-        while (saveNb1 > 0)
-        {
-            rest = saveNb1 % 2; // Stock le reste de l'entier saisi, donc 0 ou 1
-            p = pow(10, pow2); // Calcul les puissances de 2
-            binary = binary + rest * p; // Calcul le nombre binaire
-            ++pow2; // Incrémente de 1 la puissance de 2
-            saveNb1/=2; // Divise l'entier saisi par 2 et vérifie si celui ci est supérieur à 0
+        while (saveNb1 > 0) {
+            rest = saveNb1 % 2;
+            saveNb1 /= 2;
+            if(rest == 0)
+                counter0++;
+            else
+                counter1++;
+            i++;
+        }
+        if(i < 16){
+            missing0 = 16 - i;
+            counter0 += missing0;
+        }
+
+        if(counter0 == counter1){
+            temp1 = nb1;
+            if(temp1 != 0)
+                printf("%d, ", nb1);
         }
         nb1++;
-        do {
-            unsigned int modulo;
-            modulo = binary%10;
-            binary -= modulo;
-            binary /= 10;
-            if(modulo == 1)
-                counter1++;
-            else
-                counter0++;
-
-        } while (binary != 0);
-        if(counter1 == counter0)
-            printf("%u,", nb1);
     } while (nb1 <= nb2);
 
 
-    return 0;
 
+
+    return 0;
 }
