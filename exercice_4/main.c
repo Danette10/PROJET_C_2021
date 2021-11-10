1e9a921f2b2542525f5136a4bc0100e90b4c5a42
Danette10 <38246813+Danette10@users.noreply.github.com>
2021-11-10 19:57:30 +0100

    Update main.c

--- exercice_4/main.c
+++ exercice_4/main.c
@@ -2,13 +2,17 @@
 
 int main() {
     int nb1, nb2;
+    int rest, i, counter0, counter1, missing0, saveNb1, temp1, temp2, j = 0;
     printf("Saisir un premier entier : \n");
     scanf("%d", &nb1);
     printf("Saisir un second entier :\n");
     scanf("%d", &nb2);
 
     do {
-        int rest, i = 0, counter0 = 0, counter1 = 0, missing0, saveNb1, temp1 = 0;
+        counter0 = 0;
+        counter1 = 0;
+        i = 0;
+
         saveNb1 = nb1;
         while (saveNb1 > 0) {
             rest = saveNb1 % 2;
@@ -23,17 +27,25 @@ int main() {
             missing0 = 16 - i;
             counter0 += missing0;
         }
-
         if(counter0 == counter1){
+            temp2 = temp1;
             temp1 = nb1;
-            if(temp1 != 0)
-                printf("%d, ", nb1);
+
+            if(j != 0){
+                if(j == 1){
+                    printf("%d", temp2);
+                }else if(j != 1)
+                    printf(",%d ", temp2);
+
+            }
+            j++;
+
         }
         nb1++;
+
     } while (nb1 <= nb2);
 
-
-
+    printf("et %d\n", temp1);
 
     return 0;
 }
