091c94318fb8349256e0b5ba7c66ef3fdf66d918
Danette10 <38246813+Danette10@users.noreply.github.com>
2021-11-14 18:24:38 +0100

    Update main.c

--- exercice_5/main.c
+++ exercice_5/main.c
@@ -1,82 +1,54 @@
 #include <stdio.h>
+#include <stdlib.h>
 /*
- * Exercice n°5 du projet de C
- * par Dan Sebag
- * le 07/11/2021
+ * Calcul du salaire brut d'un commercial
+ * Par Dan Sebag
+ * Le 04/11/2021
  */
 int main() {
-    char retry;
+    char restart;
+
     do {
-        double salary, turnover, commission, moneyTraveler;
-        unsigned int kilometers, moneyKilometers, traveler, dayTraveler, howSell;
+        double salary, resultSalary, resultKilometers;
+        unsigned int travelers, nothingSold, dayWork, salaryDayWork, kilometers;
 
-        printf("Saisir votre salaire :\n");
+        printf("Saisir votre salaire mensuel:\n");
         scanf("%lf", &salary);
-        printf("Saisir le chiffre d'affaire du representant :\n");
-        scanf("%lf", &turnover);
-        printf("Combien de km avez vous parcouru ?\n");
+        printf("Combien de km avez vous parcourus ?\n");
         scanf("%u", &kilometers);
-
-        moneyKilometers = 0.50 * kilometers;
-
-        printf("Avez vous ete envoye a l'etranger ? (1 = oui | 0 = non)\n");
-        scanf("%u", &traveler);
-        if(turnover <= 13000){
-            commission = turnover * 0.016;
-            salary += commission;
-        }else if(turnover <= 22000){
-            commission = turnover * 0.022;
-            salary += commission;
-        }else{
-            commission = turnover * 0.03;
-            salary += commission;
-        }
-        if(moneyKilometers >= 50 && moneyKilometers <= 250)
-            salary += moneyKilometers;
+        printf("Avez vous vendu ce mois ci ? (1 = oui | 0 = non)\n");
+        scanf("%u", &nothingSold);
+        printf("Avez vous ete envoyes a l'etranger ? (1 = oui | 0 = non)\n");
+        scanf("%u", &travelers);
+        if(salary <= 13000)
+            resultSalary = salary * (1-0.016);
+        else if(salary <= 22000)
+            resultSalary = salary * (1-0.022);
         else
-            salary = salary;
-        switch (traveler) {
+            resultSalary = salary * (1-0.003);
+        resultKilometers = kilometers * 0.50;
+        if (resultKilometers >= 50 && resultKilometers <= 250)
+            resultSalary += resultKilometers;
+        switch (travelers && nothingSold) {
             case 1:
-                printf("Combien de jours avez vous voyager ?\n");
-                scanf("%u", &dayTraveler);
-                moneyTraveler = dayTraveler * 100;
-                salary += moneyTraveler;
-
-                printf("Avez vous vendu ce mois ci ? (1 = oui | 0 = non)\n");
-                scanf("%u", &howSell);
-                switch (howSell) {
-                    case 1:
-                        printf("Votre salaire brut mensuel est de %.2lf euros\n", salary);
-                        break;
-                    case 0:
-                        salary += 100;
-                        printf("Votre salaire brut mensuel est de %.2lf euros\n", salary);
-                        break;
-                }
-
+                printf("Combien de jours avez vous ete envoyes a l'etranger ?\n");
+                scanf("%u", &dayWork);
+                salaryDayWork = dayWork * 100;
+                resultSalary += salaryDayWork;
+                printf("Votre salaire brut mensuel est de %.2lf euros", resultSalary);
                 break;
-
             case 0:
-                printf("Avez vous vendu ce mois ci ? (1 = oui | 0 = non)\n");
-                scanf("%u", &howSell);
-                switch (howSell) {
-                    case 1:
-                        printf("Votre salaire brut mensuel est de %.2lf euros\n", salary);
-                        break;
-                    case 0:
-                        salary += 100;
-                        printf("Votre salaire brut mensuel est de %.2lf euros\n", salary);
-                        break;
-                }
-
+                resultSalary - 100;
+                printf("Votre salaire brut mensuel est de %.2lf euros", resultSalary);
                 break;
         }
+
         fflush(stdin);
-        printf("Voulez vous recommencer ? (o = oui | O = oui)\n");
-        scanf("%c", &retry);
+        printf("\nVoulez vous recommencer ? (o = oui | O = oui)\n");
+        scanf("%c", &restart);
+    } while (restart == 'o' || restart == 'O');
 
-    } while (retry == 'o' || retry == 'O');
 
-
+    system("PAUSE");
     return 0;
 }
