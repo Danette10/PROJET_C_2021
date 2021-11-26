86cf557e72e72ed26358cc40e1fc3a005db269d4
Danette10 <38246813+Danette10@users.noreply.github.com>
2021-11-26 10:38:35 +0100

    Update main.c

--- exercice_4/main.c
+++ exercice_4/main.c
@@ -1,57 +1,69 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define OCTET16 65536
 
 int main() {
-    int nb1, nb2;
-    int rest, i, counter0, counter1, missing0, nb1Bis, temp1, temp2, j = 0, saveNb1;
-    printf("Saisir un premier entier : \n");
-    scanf("%d", &nb1);
-    printf("Saisir un second entier :\n");
-    scanf("%d", &nb2);
-    saveNb1 = nb1;
-
+    char restart;
     do {
-        counter0 = 0;
-        counter1 = 0;
-        i = 0;
-
+        int nb1, nb2;
+        int i, counter0, counter1, missing0, temp1, temp2, j = 0, nb1Bis, tempNb1;
+        long saveNb1, rest;
+        printf("Saisir un premier entier : \n");
+        scanf("%d", &nb1);
+        printf("Saisir un second entier :\n");
+        scanf("%d", &nb2);
+        if(nb1 >= nb2){
+            tempNb1 = nb1;
+            nb1 = nb2;
+            nb2 = tempNb1;
+        }
         nb1Bis = nb1;
-        while (nb1Bis > 0) {
-            rest = nb1Bis % 2;
-            nb1Bis /= 2;
-            if(rest == 0)
-                counter0++;
-            else
-                counter1++;
-            i++;
-        }
-        if(i < 16){
-            missing0 = 16 - i;
-            counter0 += missing0;
-        }
-        if(counter0 == counter1){
-            temp2 = temp1;
-            temp1 = nb1;
+        do {
+            counter0 = 0;
+            counter1 = 0;
+            i = 0;
+            if(nb1 < 0)
+                saveNb1 = nb1 + OCTET16;
+            else if(nb1 > 0)
+                saveNb1 = nb1;
 
-            if(j != 0){
-                if(j == 1){
-                    printf("%d ", temp2);
-                }else if(j != 1)
-                    printf(",%d ", temp2);
-
+            while (saveNb1 > 0) {
+                rest = saveNb1 % 2;
+                saveNb1 /= 2;
+                if(rest == 0)
+                    counter0++;
+                else
+                    counter1++;
+                i++;
             }
-            j++;
+            if(i < 16){
+                missing0 = 16 - i;
+                counter0 += missing0;
+            }
+            if(counter0 == counter1){
+                temp2 = temp1;
+                temp1 = nb1;
+                if(j != 0){
+                    if(j == 1){
+                        printf("%d", temp2);
+                    }else if(j != 1)
+                        printf(",%d ", temp2);
+                }
+                j++;
+            }
+            nb1++;
+        } while (nb2 >= nb1);
+        if(j == 0)
+            printf("Il n'y a pas de nombres sympathiques entre %d et %d", nb1Bis, nb2);
+        else if(j == 1)
+            printf("%d\n", temp1);
+        else if(j > 1)
+            printf(" et %d", temp1);
+        fflush(stdin);
+        printf("\nVoulez vous recommencer ? (o = oui | O = oui)\n");
+        scanf("%c", &restart);
+    } while (restart == 'o' || restart == 'O');
 
-        }
-        nb1++;
-
-    } while (nb1 <= nb2);
-    if(j == 0)
-        printf("Il n'y a aucun nombre sympathiques entre %d et %d", saveNb1, nb2);
-    else if(j == 1)
-        printf("%d", temp1);
-    else if (j >= 2)
-        printf("et %d\n", temp1);
-
+    system("PAUSE");
     return 0;
 }
