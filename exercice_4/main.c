ac808a01ad297ed5066e0629921347855368c5cd
Danette10 <38246813+Danette10@users.noreply.github.com>
2021-11-27 09:10:01 +0100

    Update main.c

--- exercice_4/main.c
+++ exercice_4/main.c
@@ -12,29 +12,48 @@ int main() {
         scanf("%d", &nb1);
         printf("Saisir un second entier :\n");
         scanf("%d", &nb2);
-        if(nb1 >= nb2){
+        nb1Bis = nb1; // Sauvegarde la valeur de nb1 pour l'affichage
+
+        if(nb1 >= nb2){ // Si nb1 est bien sup ou égale a nb2 on execute ce qui se trouve en dessous
+
             tempNb1 = nb1;
             nb1 = nb2;
             nb2 = tempNb1;
         }
-        nb1Bis = nb1;
-        do {
-            counter0 = 0;
-            counter1 = 0;
-            i = 0;
-            if(nb1 < 0)
-                saveNb1 = nb1 + OCTET16;
-            else if(nb1 > 0)
-                saveNb1 = nb1;
 
-            while (saveNb1 > 0) {
-                rest = saveNb1 % 2;
-                saveNb1 /= 2;
-                if(rest == 0)
+        do {
+            counter0 = 0; // Compteur de 0
+
+            counter1 = 0; // Compteur de 1
+
+            i = 0; // Compteur d'octet
+
+            if (nb1 < 0){ // Pour le cas ou nb1 est négatif
+
+                saveNb1 = nb1 + OCTET16; // Transforme nb1 négatif en nb1 possitif sur 16 octets
+
+            }else if(nb1 > 0) { // Si nb1 est possitif
+
+                saveNb1 = nb1; // On met tout simplement la valeur de nb1 dans saveNb1
+            }
+
+            while (saveNb1 > 0) { // La boucle While va convertir l'entier décimal en nombre binaire
+
+                rest = saveNb1 % 2; // Calcul le rest qui sera égal sois à 1 sois à 0
+
+                saveNb1 /= 2; // On divise par deux pour re calculer le rest une nouvelle fois
+
+                if(rest == 0) {
+
+                    // Si le rest = 0 alors on va ajouter 1 à counter0
+
                     counter0++;
-                else
+                }
+                else {
+                    // Si le rest = 1 alors on va ajouter 1 à counter1
                     counter1++;
-                i++;
+                }
+                i++; // Ajouter 1 au compteur de octets
             }
             if(i < 16){
                 missing0 = 16 - i;
