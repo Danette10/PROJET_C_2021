b18b27ab1dfa43816e3539fbd679a709ba11c70a
Danette10 <38246813+Danette10@users.noreply.github.com>
2021-11-28 14:15:49 +0100

    Update main.c

--- exercice_1/main.c
+++ exercice_1/main.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 /*
- * Exercice 1 du projet de C
- * le 30/10/2021
- * par Dan Sebag
+ * Faire un papillon de hauteur saisi
+ * Par Dan Sebag, Steven Wong et Quentin Jolli
  */
 int main() {
     char retry;
     do {
-        int nb, i,j, impair, pair;
+        int nb, i, j, impair, pair;
         printf("Saisir un entier :\n");
         scanf("%d", &nb);
-        for (i = 0; i < nb - i - 1; i++){
-            for (j = 0; j < nb; j++){
+
+        // Première partie du dessin
+        for (i = 0; i < nb - i - 1; i++){ // Boucle for qui va permettre de calculer le nombre
+                                          // d'espace entre le premier et le deuxième triangle
+
+            for (j = 0; j < nb; j++){ // Boucle d'affichage des * ou des espaces
+
                 if(j <= i || j >= nb - i -1){
                     printf("*  ");
                 } else{
@@ -22,8 +26,8 @@ int main() {
             }
             printf("\n");
         }
-        for(impair=0;impair <= nb && nb%2 != 0;impair++)
-        {
+        for(impair=0;impair <= nb && nb%2 != 0;impair++) {
+            // Si nb est impair alors on va sauter une ligne est afficher une ligne * suplémentaire
 
             if(impair == nb){
                 printf("\n");
@@ -31,14 +35,17 @@ int main() {
                 printf("*  ");
         }
 
-        for(pair=0;pair <= nb && nb%2 == 0;pair++)
-        {
-
+        for(pair=0;pair <= nb && nb%2 == 0;pair++) {
+            // Si nb est pair alors on va sauter une ligne est afficher une ligne * suplémentaire
             if(pair == nb){
                 printf("\n");
             } else
                 printf("*  ");
         }
+
+        // Deuxième partie du dessin
+
+        // For qui va partir de i = nb - i - 1 donc il va se créé à partir de la fin du for précédent
 
         for (i = nb - i - 1; i > 0; i--){
             for (j = nb; j > 0; j--){
@@ -55,7 +62,7 @@ int main() {
         printf("Voulez vous recommencer ? (o = oui | O = oui)\n");
         scanf("%c", &retry);
     } while (retry == 'o' || retry == 'O');
-    
+
     system("PAUSE");
 
     return 0;
